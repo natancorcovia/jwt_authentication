4e317b35818838d9b627d502671d47a0cd51b189
Natan Corcovia <natankorkov@gmail.com>
2025-12-27 14:51:08 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+
+const app = express();
+const PORT = 3000;
+const SECRET_KEY = "9885f8af04289135df259e34bd22d17fe45ea81e";
+
+app.use(express.json());
+
+const users = [
+  { id: 1, username: "natansilva", password: "123456", role: "admin" },
+  { id: 2, username: "natancorcovia", password: "123456", role: "user" },
+];
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (user) {
+    const token = jwt.sign(
+      { id: user.id, username: user.username, role: user.role },
+      SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    res.status(201).json({ massage: token });
+  } else {
+    res.status(400).json({ message: "Usuário ou senha inválidos!" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor online!");
+});
